7c1e57819e65ef55fbeaeda03f4156b880fb5bae
kaish ibn sufian <kaish.sufian@northsouth.edu>
2021-09-03 08:04:00 +0600

    join class function created

--- create_join_class/views.py
+++ create_join_class/views.py
@@ -11,3 +11,21 @@ from .forms import CreateClassRoomForm, ReadingMaterialForm
 from .models import *
 
 
+# This method is used to create class. It creates a class with the POST data retrieved from the user via form
+# and also generates a unique class code for other users to join.
+@login_required
+def create_class(request):
+    if request.method == "GET":
+        return render(request, 'create_join_class/create_class.html', {'form': CreateClassRoomForm})
+    else:
+        try:
+            form_data = CreateClassRoomForm(request.POST)
+            new_class = form_data.save(commit=False)
+            new_class.teacher = request.user
+            new_class.classCode = uuid.uuid4().hex[:6].upper()
+            new_class.save()
+            return redirect('home_classroom')
+        except ValueError:
+            return render(request, 'create_join_class/create_class.html',
+                          {'form': CreateClassRoomForm, 'error': 'Bad data passed in. Try again!'})
+
