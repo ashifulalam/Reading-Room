6a77af72d4a1b750bb209c7ea441962443ac345f
Ashiful Alam <shuuvvo@gmail.com>
2021-09-04 12:19:37 +0600

    Reading material method created
    # This method is used to upload reading material by the teacher of the class. The teacher
    # can only upload pdf files as reading material. If upload is successful, the teacher
    # is redirected to the created classroom html. Else the teacher is prompted to upload the file again.

--- create_join_class/views.py
+++ create_join_class/views.py
@@ -28,4 +28,30 @@ def create_class(request):
         except ValueError:
             return render(request, 'create_join_class/create_class.html',
                           {'form': CreateClassRoomForm, 'error': 'Bad data passed in. Try again!'})
+                          
+
+# This method is used to upload reading material by the teacher of the class. The teacher
+# can only upload pdf files as reading material. If upload is successful, the teacher
+# is redirected to the created classroom html. Else the teacher is prompted to upload the file again.
+@login_required
+def uploadReadingMaterial(request, classroom_pk):  #AA
+    if request.method == 'GET':
+        try:
+            classroom = ClassRoom.objects.get(teacher=request.user, pk=classroom_pk)
+        except ClassRoom.DoesNotExist:
+            return render(request, "create_join_class/uploadReadingMaterial.html",
+                          {'error': 'You don\'t have upload permissions to this classroom!'})
+        form = ReadingMaterialForm()
+        return render(request, "create_join_class/uploadReadingMaterial.html", {'form': form})
+    else:
+        form = ReadingMaterialForm(request.POST, request.FILES)
+        if form.is_valid():
+            newmaterial = form.save(commit=False)
+            newmaterial.classroom = ClassRoom(pk=classroom_pk)
+            newmaterial.uploader = User(request.user.id)
+            newmaterial.save()
+            messages.success(request, 'File Upload Successful')
+            return redirect('viewCreatedReadingMaterial', classroom_pk)
+        else:
+            return render(request, "create_join_class/uploadReadingMaterial.html", {'form': form})
 
